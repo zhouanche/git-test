8daf78a75e2593426391fbe54b7244e4166b1293
周安 <anche521@126.com>
2020-03-15 15:14:30 +0800

    把本地工程添加到本地仓库

---
+++ src/com/itheima/git/FirstClass.java
@@ -0,0 +1,18 @@
+package com.itheima.git;
+
+/**
+ * @author:
+ * @date 2020/3/15 - 15:04
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+
+}
